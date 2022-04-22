79570ca5129e2fd3d211eb2d0ee0c4984fa280e0
Alvin Wanjohi <akwanjohi99@gmail.com>
2022-04-22 17:31:47 +0300

    Implemented google search feature.
    - Removed mic icon from search input.
    - Changed font to use 'Poppins'
    - Refactored css.
    - Refactored js.

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
-let search = document.getElementById('search')
-let searchIcon = document.getElementById('searchIcon')
+let search = document.getElementById("search");
+let searchIcon = document.getElementById("searchIcon");
 
-searchIcon.onclick = function () {
-    search.classList.toggle('active')
+searchIcon.onclick = () => {
+  search.classList.toggle("active");
+};
+
+let page = document.getElementById("page");
+let themeIcon = document.getElementById("themeIcon");
+let themeName = document.getElementById("themeName");
+let toggle = document.getElementById("toggle");
+
+let localData = localStorage.getItem("myTheme");
+
+if (localData == null) {
+  localStorage.setItem("myTheme", "light");
 }
 
-let page = document.getElementById('page')
-let themeIcon = document.getElementById('themeIcon')
-let themeName = document.getElementById('themeName')
-let toggle = document.getElementById('toggle')
-
-if (localStorage.getItem('myTheme') == null) {
-    localStorage.setItem('myTheme', 'light')
+if (localData == "light") {
+  page.classList.remove("dark-theme");
+  themeIcon.classList.remove("fa-moon");
+  themeIcon.classList.add("fa-sun");
+  themeName.innerText = "Light";
+} else if (localData == "dark") {
+  page.classList.add("dark-theme");
+  themeIcon.classList.remove("fa-sun");
+  themeIcon.classList.add("fa-moon");
+  themeName.innerText = "Dark";
 }
 
-let localData = localStorage.getItem('myTheme')
-
-if (localData == 'light') {
-    page.classList.remove('dark-theme')
-    themeIcon.classList.remove('fa-moon')
-    themeIcon.classList.add('fa-sun')
-    themeName.innerHTML = 'Light'
-
-} else if (localData == 'dark') {
-    page.classList.add('dark-theme')
-    themeIcon.classList.remove('fa-sun')
-    themeIcon.classList.add('fa-moon')
-    themeName.innerHTML = 'Dark'
-}
-
-toggle.onclick = function () {
-    if (themeIcon.classList.contains('fa-sun')){
-        themeIcon.classList.remove('fa-sun')
-        themeIcon.classList.add('fa-moon')
-        themeName.innerHTML = 'Dark'
-        page.classList.toggle('dark-theme')
-        localStorage.setItem('myTheme', 'dark')
-
-    }
-    else if (themeIcon.classList.contains('fa-moon')) {
-        themeIcon.classList.remove('fa-moon')
-        themeIcon.classList.add('fa-sun')
-        themeName.innerHTML = 'Light'
-        page.classList.toggle('dark-theme')
-        localStorage.setItem('myTheme', 'light')
-
-    }
-}
+toggle.onclick = () => {
+  if (themeIcon.classList.contains("fa-sun")) {
+    themeIcon.classList.remove("fa-sun");
+    themeIcon.classList.add("fa-moon");
+    themeName.innerText = "Dark";
+    page.classList.toggle("dark-theme");
+    localStorage.setItem("myTheme", "dark");
+  } else if (themeIcon.classList.contains("fa-moon")) {
+    themeIcon.classList.remove("fa-moon");
+    themeIcon.classList.add("fa-sun");
+    themeName.innerText = "Light";
+    page.classList.toggle("dark-theme");
+    localStorage.setItem("myTheme", "light");
+  }
+};
